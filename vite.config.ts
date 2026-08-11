0cefbbb3f98f1a8cf7238872e756a89a4c08e598
Mohit Sundrani <mohit4sundrani@gmail.com>
2026-08-12 01:55:36 +0530

    Update dependencies and switch to vite-tsconfig-paths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    resolve: {
-        tsconfigPaths: true,
-    },
-    plugins: [tanstackStart(), viteReact(), tailwindcss()],
+    plugins: [tanstackStart(), viteReact(), tailwindcss(), viteTsconfigPaths()],
 });
